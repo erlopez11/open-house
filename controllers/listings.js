a24fe176f9206123aed2182c0a6bb8a884d4def4
erlopez11 <lopezelainy@gmail.com>
2025-03-24 18:10:13 -0500

    wip: define route to all listings page

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,4 +3,27 @@ const router = express.Router();
 const Listing = require('../models/listing');
 
 
+router.get('/', async (req, res) => {
+    try {
+        const allListings = await Listing.find({});
+        console.log(allListings);
+        res.render('listings/index.ejs')
+        
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
