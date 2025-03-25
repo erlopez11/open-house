47962752308df0c8305740d253f18f57ea975fcd
erlopez11 <lopezelainy@gmail.com>
2025-03-24 19:17:18 -0500

    define route to add new listing, update index route to view all listings

--- controllers/listings.js
+++ controllers/listings.js
@@ -5,10 +5,26 @@ const Listing = require('../models/listing');
 
 router.get('/', async (req, res) => {
     try {
-        const allListings = await Listing.find({});
-        console.log(allListings);
-        res.render('listings/index.ejs')
+        const populatedListings = await Listing.find({}).populate('owner');
+        res.render('listings/index.ejs', {
+            listings: populatedListings,
+        });
         
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+router.get('/new', (req, res) => {
+    res.render('listings/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+    try {
+        req.body.owner = req.session.user._id;
+        await Listing.create(req.body);
+        res.redirect('/listings');
     } catch (error) {
         console.log(error);
         res.redirect('/');
