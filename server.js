864c36c24ea60a0755f0aa4c71dcd15027c15d61
erlopez11 <lopezelainy@gmail.com>
2025-03-24 19:18:03 -0500

    fix: rmv extra app.use listings controller

--- server.js
+++ server.js
@@ -41,9 +41,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
-app.use('/listing', isSignedIn, listingsController);
-
-app.use('/listings', listingsController);
+app.use('/listings', isSignedIn, listingsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
